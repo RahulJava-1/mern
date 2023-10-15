f07a5aa002022b952cf0518f2f9e4e8e5f9237f8
RahulJava-1 <rahultripathirudra@gmail.com>
2023-10-15 20:20:33 +0530

    s commit

--- server/app.js
+++ server/app.js
@@ -1,11 +1,33 @@
-const express = require('express')
+const mongoose = require('mongoose');
+const express = require('express');
 const app = express();
+
+const DB = 'mongodb+srv://rahul:rahul123@cluster0.afakb9j.mongodb.net/mernstack?retryWrites=true&w=majority';
+
+mongoose.connect(DB,{
+
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+
+}).then(()=>{
+    console.log(`Connected`);
+}).catch((err)=>console.log(`Not connected`));
+
+
+//middleware
+const middleware = (req,res,next)=>{
+    console.log(`This is Middleware`);
+    //next();
+}
+
 
 app.get('/',(req, res) =>{
     res.send(`Hello World from server`);
 });
 
-app.get('/about',(req,res) =>{
+app.get('/about', middleware, (req,res) =>{
     res.send(`About`);
 });
 
